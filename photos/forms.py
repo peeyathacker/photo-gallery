bbb806c46c6da6982dbea3bed95361f7f086026e
dhairya903 <dhairya24@somaiya.edu>
2021-12-03 13:48:15 +0530

    ...

--- photos/forms.py
+++ photos/forms.py
@@ -11,8 +11,8 @@ class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
         self.fields['username'].widget.attrs.update(
-            {'class': 'form-control', 'placeholder': 'Enter username...'})
+            {'class': 'form-control', 'placeholder': 'Enter username'})
         self.fields['password1'].widget.attrs.update(
-            {'class': 'form-control', 'placeholder': 'Enter password...'})
+            {'class': 'form-control', 'placeholder': 'Enter password'})
         self.fields['password2'].widget.attrs.update(
-            {'class': 'form-control', 'placeholder': 'Confirm password...'})
+            {'class': 'form-control', 'placeholder': 'Confirm password'})
